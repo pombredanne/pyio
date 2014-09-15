7839b5c89898a590ea045388eabc5cd9706f62aa
William Kettler <william.p.kettler@gmail.com>
2014-09-14 19:34:44 -0500

    Added threading.

--- bin/r_loop.py
+++ bin/r_loop.py
@@ -14,7 +14,7 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
@@ -23,13 +23,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import argparse
+import time
+import threading
 from random import randint
 from pyio import r_seq
+
+
+alive = True
+
 
 def walk(root):
     """
     Walk directory and return absolute path for all files.
-    
+
     Inputs:
         root (str): Root directory
     Outputs:
@@ -37,38 +43,75 @@ def walk(root):
     """
     if not os.path.isdir(root):
         raise ValueError('%s is not a directory' % root)
-    
+
     f = []
     for root, dirs, files in os.walk(root):
         for name in files:
             f.append(os.path.join(root, name))
     return f
 
-def main(root, bs):
+
+def read(files, count, bs):
+    """
+    Read a random file.
+
+    Inputs:
+        files (list): File list
+        count  (int): File count
+        bs     (int): Block size
+    Outputs:
+        None
+    """
+    id = threading.current_thread()
+    while alive:
+        f = files[randint(0, count)]
+        print "%s %s" % (id, f)
+        r_seq(f, bs)
+
+
+def main(root, bs, thr_ct):
     """
     Infinite read loop.
-    
+
     Inputs:
         root (str): Root directory
         bs   (int): Block size in KB
     Outputs:
         NA
     """
-    
+
     # Walk directory
     files = walk(root)
-    
-    while True:
-        x = randint(0, len(files)-1)
-        f = files[x]
-        print 'Reading "%s".' % f
-        r_seq(f, bs)
+    count = len(files) - 1
+
+    print "Use CTRL-C to exit."
+
+    # Start threads
+    thrs = []
+    for i in range(thr_ct):
+        t = threading.Thread(target=read, args=(files, count, bs))
+        t.start()
+        thrs.append(t)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        global alive
+        alive = False
+
+    # Wait for threads to finish
+    for t in thrs:
+        t.join()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Infinite read loop.')
     parser.add_argument('--dir', '-d', dest='dir', type=str, required=True,
-                    help='Root directory')
-    parser.add_argument('--bs', dest='bs', type=int, required=False, default=32,
-                    help='IO block size in KB')
+                        help='Root directory')
+    parser.add_argument('--bs', dest='bs', type=int, required=False,
+                        default=32, help='IO block size in KB')
+    parser.add_argument('--threads', '-t', dest='thr_ct', type=int,
+                        required=False, default=1, help='Thread count')
     args = parser.parse_args()
-    main(args.dir, args.bs)
+    main(args.dir, args.bs, args.thr_ct)
